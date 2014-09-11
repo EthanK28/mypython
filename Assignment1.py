83fd4fac7a0a6ed1a981cc8fcfae80c3b26f83cb
Ethan_kang <kes0228@gmail.com>
2014-09-11 15:47:17 +0900

    no message

---
+++ Assignment1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#1번
+"""
+a = "Hello Python"
+a = a[6:] +' '+ a[0:6]
+print a
+"""
+
+#2번
+"""
+b = "Hello Python"
+b = b[5:] +' '+ b[0:6]
+b = 'World'+b
+print b
+"""
+
+#3번
+"""
+c="Hello"
+c=c[::-1]
+print c
+"""
+
+#4번
+
+#5번
+for x in range(1,101):
+    if(x%2 == 1):
+        print x,
+    else:
+        continue
+
+#6번
+sum = 0
+while 
